0fa118378da18576cf04f9e7487ab1eab560a9a2
AlexKwest <lex.kaname@gmail.com>
2020-01-15 01:32:05 +0300

    add header AngularJS

---
+++ front-end-app/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegComponent } from './reg/reg.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoute: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'reg', component: RegComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'dashboard', component:DashboardComponent}
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegComponent,
+    AuthComponent,
+    DashboardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
